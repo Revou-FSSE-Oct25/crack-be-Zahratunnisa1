b9cd74217f06d932095ad50196acf88b9064d11d
Zahra <zahranabintang2@gmail.com>
2026-04-03 13:22:55 +0800

    Edit UI more beauty

--- flight-booking-backend/src/auth/auth.controller.ts
+++ flight-booking-backend/src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('auth') 
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login') 
+  login(@Body() body: any) {
+    const user = { id: 1, email: body.email };
+    return this.authService.login(user);
+  }
+}
