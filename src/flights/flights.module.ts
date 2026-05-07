87ab1806bc22fc33dec7c67f79d830c9a08f8274
Zahra <zahranabintang2@gmail.com>
2026-05-07 19:48:11 +0800

    buat e-booking ticket

--- src/flights/flights.module.ts
+++ src/flights/flights.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { FlightsService } from './flights.service';
 import { FlightsController } from './flights.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [FlightsController],
   providers: [FlightsService, PrismaService],
+})
+
+@Module({
+  imports: [AuthModule], // 🔥 TAMBAHKAN INI
+  controllers: [FlightsController],
+  providers: [FlightsService],
 })
 export class FlightsModule {}
 
